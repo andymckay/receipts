9d35e40b87488af6371d4c6a37c068f08f85bcce
Andy McKay <amckay@mozilla.com>
2012-04-19 14:51:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='receipts',
-    version='0.1.3',
+    version='0.1.4',
     description='Verify web app receipts',
     long_description=open('readme.rst').read(),
     author='Andy McKay',
