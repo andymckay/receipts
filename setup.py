f1f4ab0cd2d8bdfb8311ce971079bccc2408a4aa
Andy McKay <amckay@mozilla.com>
2012-04-25 13:56:47 -0700

    new structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='receipts',
-    version='0.1.4',
+    version='0.1.5',
     description='Verify web app receipts',
     long_description=open('readme.rst').read(),
     author='Andy McKay',
